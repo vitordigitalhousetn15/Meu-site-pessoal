e5b3c9733731a3623bda66ac97c555f91e94fc32
Vitor Costa <vitor.costa@prontmed.com>
2021-02-18 18:30:51 -0300

    add: rota com post para produtos

--- routes/products.js
+++ routes/products.js
@@ -3,22 +3,30 @@ const model = require('../model/products')
 
 var router = express.Router();
 
+const types = [{
+  id: "eletronic",
+  label: "Eletronico"
+}, {
+  id: "food",
+  label: "Alimenticio"
+}, {
+  id: "drugs",
+  label: "Farmaceutico"
+}];
+
+const pageName = "Cadastro de produtos"
+
 // http://localhost:3000/products
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const types = [{
-    id: "eletronic",
-    label: "Eletronico"
-  }, {
-    id: "food",
-    label: "Alimenticio"
-  }, {
-    id: "drugs",
-    label: "Farmaceutico"
-  }];
 
-  res.render('products', { name: "Cadastro de produtos", types: types });
+  res.render('products', { name: pageName, types: types });
 });
+
+router.post("/", function(req, res) {
+  console.log(req.body);
+  res.render('products', { successSave: true, name: pageName, types: types })
+})
 
 module.exports = router;
